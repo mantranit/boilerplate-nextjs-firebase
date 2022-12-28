94dd45fd1270944a691bf85b69da883fe49ad521
Man Tran <tranduyminhman@gmail.com>
2022-12-28 15:55:38 +0700

    wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
-import '../styles/globals.scss';
+import '../styles/global.scss';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
